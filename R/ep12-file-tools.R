1a4deed77bffb92c852b8637ceceb9d42d6ecd64
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-09-15 10:06:47 -0400

    added UnixizeMotionPath()

---
+++ R/ep12-file-tools.R
@@ -0,0 +1,11 @@
+# ep12-file-tools.R
+
+#' Replace common directory locations like Z:\ with the MotionAnalysis root directory
+#' @export
+UnixizeMotionPath = function(x){
+  x = gsub("Z:\\groups\\", "", x, fixed = T)
+  x = gsub(":\\groups\\", "", x, fixed = T)
+  x = gsub("\\", "/", x, fixed = T)
+  x = gsub("/groups/MotionAnalysis", "MotionAnalysis", x)
+  return(x)
+}
